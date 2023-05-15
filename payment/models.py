b91b66a39e19cdc31e03cd5e475fcfdee1156957
aliteymouri <aliteymr@gmail.com>
2023-05-15 09:31:35 +0330

    create Order-model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from accounts.models import User
+from video.models import Video
+
+
 # Create your models here.
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
+
+    class Meta:
+        verbose_name = 'سفارش'
+        verbose_name_plural = 'سفارش ها'
+        ordering = ('-created_at',)
